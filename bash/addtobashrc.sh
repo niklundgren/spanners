fdb736be7916bb236041c83f3a2c00914744c896
Nik W. Lundgren <niklundgren12@gmail.com>
2026-01-22 17:05:03 -0800

    Updating default ls behavior to group directories first because it looks nice :) Importing hdf5 in startup.py since current research uses hdf5 archives.

--- bash/addtobashrc.sh
+++ bash/addtobashrc.sh
@@ -60,7 +60,7 @@ function binlink {
 export LS_COLORS=${LS_COLORS}:"fi=0;37:"
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    alias ls='ls --color=auto --group-directories-first'
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
     alias grep='grep --color=auto'
